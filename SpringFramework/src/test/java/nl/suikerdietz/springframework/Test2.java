b7b09c47fca6a5c09a36f25529ab3fe80fbc06d0
= <remco@suikerdietz.nl>
2017-06-07 13:29:06 +0200

    Extra unittest (with extra testfile)

---
+++ SpringFramework/src/test/java/nl/suikerdietz/springframework/Test2.java
@@ -0,0 +1,14 @@
+package nl.suikerdietz.springframework;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class Test2 {
+
+	@Test
+	public static void Test()
+	{
+		assertEquals(1, 2);
+	}
+}
